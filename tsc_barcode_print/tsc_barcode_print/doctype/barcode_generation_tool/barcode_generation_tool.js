93428020c2698c87eebd3ebfa5ef3a97e593e763
somilvaishya <somilsearchosis@gmail.com>
2026-07-13 11:46:40 +0000

    feat: add print button to barcode generation tool

--- tsc_barcode_print/tsc_barcode_print/doctype/barcode_generation_tool/barcode_generation_tool.js
+++ tsc_barcode_print/tsc_barcode_print/doctype/barcode_generation_tool/barcode_generation_tool.js
@@ -1,8 +1,26 @@
-// Copyright (c) 2026, Somil and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Barcode Generation Tool", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Barcode Generation Tool', {
+    refresh: function(frm) {
+        if (frm.doc.docstatus === 1) { // Only if submitted
+            frm.add_custom_button(__('Print Barcode'), function() {
+                if (!frm.doc.barcode_template) {
+                    frappe.msgprint("Please select a Barcode Template");
+                    return;
+                }
+                
+                frm.disable_save();
+                window.TSCPrinter.printTSPL(
+                    frm.doc.barcode_template,
+                    frm.doc.item_code,
+                    frm.doc.batch_id,
+                    frm.doc.manufacturing_date,
+                    frm.doc.label_qty,
+                    frm.doc.no_of_copies
+                ).then(() => {
+                    frm.enable_save();
+                }).catch(() => {
+                    frm.enable_save();
+                });
+            }, __('Actions'));
+        }
+    }
+});
