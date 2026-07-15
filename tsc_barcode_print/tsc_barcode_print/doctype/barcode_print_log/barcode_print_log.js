255ec843f8b99518e626520eed954cb6a0e6ac64
somilvaishya <somilsearchosis@gmail.com>
2026-07-15 05:05:41 +0000

    fix: correct comment syntax in barcode_print_log.js

--- tsc_barcode_print/tsc_barcode_print/doctype/barcode_print_log/barcode_print_log.js
+++ tsc_barcode_print/tsc_barcode_print/doctype/barcode_print_log/barcode_print_log.js
@@ -1,5 +1,5 @@
-# Copyright (c) 2026, Somil and contributors
-# For license information, please see license.txt
+// Copyright (c) 2026, Somil and contributors
+// For license information, please see license.txt
 
 frappe.ui.form.on('Barcode Print Log', {
     // Client-side interactions if needed
